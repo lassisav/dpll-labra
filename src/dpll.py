6558a08a94f815020b69efde9c70993dfedca4e9
lassisav <lassi.savolainen@helsinki.fi>
2025-11-11 11:05:08 +0200

    Luotiin minimituote, joka spagettisuudestaan ja skaalautumattomuudestaan huolimatta vaikuttaisi toimivan. Parannuksia tulossa.

--- src/dpll.py
+++ src/dpll.py
@@ -29,27 +29,76 @@ def karsinnan_alustus(lista, jakauma):
 
 def karsinta(lista, jakauma):
     """Funktio, joka toteuttaa algoritmin toistettavat toimet."""
+    print("Lähetään kattelee jakaumalla " + literaalit_merkkijonoksi(jakauma))
     ## Onnistumisen tarkistus
     if lista is None:
+        print("Onnistuttiin jakaumalla " + literaalit_merkkijonoksi(jakauma))
         return jakauma
     ## Epäonnistumisen tarkistus
     if tyhja_klausuuli(lista):
+        print("Epäonnistuttiin")
         return None
     ##TODO: Yksikköpropagaatio
     ## Literaalin valinta
     valittu = literaalin_valinta(lista)
-    ##TODO: Literaalin karsinta
-    ##TODO: Literaalin negaation karsinta
-    ## Jakauman palautus
-    return jakauma
+    print("Valittiin literaali " + str(valittu))
+    ## Yritetään literaalilla
+    yritys_lista = luo_kopio_klausuuli(lista)
+    yritys_jakauma = luo_kopio_literaali(jakauma)
+    print("Kopioitiin jakauma " + literaalit_merkkijonoksi(yritys_jakauma))
+    yritys_jakauma = lisaa_loppuun_literaali(yritys_jakauma, valittu)
+    print("Lisäystä perään " + literaalit_merkkijonoksi(yritys_jakauma))
+    yritys_lista = poista_annettu(yritys_lista, valittu)
+    print("Yritetään jakaumalla " + literaalit_merkkijonoksi(yritys_jakauma))
+    mahdollisuus = karsinta(yritys_lista, yritys_jakauma)
+    if mahdollisuus is not None:
+        return mahdollisuus
+    ## Yritetään literaalin negaatiolla
+    valittu = valittu * (-1)
+    yritys_lista = luo_kopio_klausuuli(lista)
+    yritys_jakauma = luo_kopio_literaali(jakauma)
+    yritys_jakauma = lisaa_loppuun_literaali(yritys_jakauma, valittu)
+    yritys_lista = poista_annettu(yritys_lista, valittu)
+    mahdollisuus = karsinta(yritys_lista, yritys_jakauma)
+    if mahdollisuus is not None:
+        return mahdollisuus
+    ## Palautetaan tyhjä
+    return None
+
+def poista_annettu(lista, annettu):
+    """Poistaa listasta klausuulit, joissa on annetun arvoinen literaali,
+    sekä annetun arvon negaatioiset literaalit."""
+    if lista is None:
+        return lista
+    viime = None
+    tama = lista.arvot
+    while True:
+        if tama.arvo == annettu:
+            return poista_annettu(lista.linkki, annettu)
+        if tama.arvo == (annettu * (-1)):
+            if viime is None:
+                lista.arvot = tama.linkki    
+            else:
+                viime.linkki = tama.linkki
+            lista.linkki = poista_annettu(lista.linkki, annettu)
+            return lista
+        if tama.linkki is None:
+            lista.arvot = tama.linkki
+            lista.linkki = poista_annettu(lista.linkki, annettu)
+            return lista
+        viime = tama
+        tama = tama.linkki
+
 
 def tyhja_klausuuli(lista): ## Yksikkötestit tehty
     """Tarkistaa, onko syötteenä annetussa listassa tyhjää klausuulia."""
-    if lista is None:
-        return False
-    if lista.arvot is None:
-        return True
-    return lista.linkki
+    pohja = lista
+    while True:
+        if pohja is None:
+            return False
+        if pohja.arvot is None:
+            return True
+        pohja = pohja.linkki
 
 def literaalin_valinta(lista): ## Yksikkötestit tehty
     """Saa syötteenä listan klausuuleja, ja palauttaa ensimmäisen klausuulin ensimmäisen literaalin.
@@ -94,7 +143,7 @@ def tulosta_klausuulilista(lista):
 def literaalit_merkkijonoksi(jakauma):
     """Ottaa syötteenä listan literaaleja, ja palauttaa listaa kuvaavan merkkijonon."""
     if jakauma is None:
-        return "Ei toteutuva"
+        return "Tyhjä"
     tuloste = ""
     jatkuu = True
     while jatkuu:
@@ -105,6 +154,34 @@ def literaalit_merkkijonoksi(jakauma):
             tuloste += ", "
             jakauma = jakauma.linkki
     return tuloste
+
+def luo_kopio_klausuuli(lista):
+    """Luo kopion annetusta listasta ja palauttaa sen."""
+    if lista is None:
+        return None
+    uudet_arvot = luo_kopio_literaali(lista.arvot)
+    uusi = Klausuuli(uudet_arvot)
+    uusi.linkki = luo_kopio_klausuuli(lista.linkki)
+    return uusi
+
+def luo_kopio_literaali(lista):
+    """Luo kopion annetusta listasta ja palauttaa sen."""
+    if lista is None:
+        return None
+    uusi = Literaali(lista.arvo)
+    uusi.linkki = luo_kopio_literaali(lista.linkki)
+    return uusi
+
+def lisaa_loppuun_literaali(lista, arvo):
+    """Lisää listan loppuun literaalin annetulla arvolla."""
+    if lista is None:
+        return Literaali(arvo)
+    pohja = lista
+    while True:
+        if pohja.linkki is None:
+            pohja.linkki = Literaali(arvo)
+            return lista
+        pohja = pohja.linkki
 
 if __name__=="__main__":
     main()
